2e13d523f35c14309a4641bc90ada79b649e2ab4
Joel Vega <joel5vega@gmail.com>
2025-11-07 22:08:53 +0000

    logo

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDyy8GuqG9JZBgd-PUwz49El3NXguC5PLM",
-  authDomain: "ministerios-b5a1b.firebaseapp.com",
-  projectId: "ministerios-b5a1b",
-  storageBucket: "ministerios-b5a1b.firebasestorage.app",
-  messagingSenderId: "185394749243",
-  appId: "1:185394749243:web:1220ce9e8bd6db8371e11e",
-  measurementId: "G-C9YWNHPTJ7"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID ,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId:process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
